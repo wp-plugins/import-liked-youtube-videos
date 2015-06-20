c212e9b1130ab414da827c057b601c6d2d1cacd4
Hudson Atwell <Hudson Atwell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-20 04:23:27 +0000

    adding 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/import-liked-youtube-videos/tags/1.0.2@1184553 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/cron.php
@@ -0,0 +1,120 @@
+<?php
+
+class Youtube_Liked_Cron {
+	static $history;
+	static $settings;
+
+	function __construct() {
+		/* add debug handler for cronjob */
+		add_action( 'admin_init' , array( __CLASS__  , 'debug_cron' ) ) ;
+		
+		/* add cronjob */
+		add_action( 'check_for_liked_videos', array( __CLASS__ , 'check_for_liked_videos') );
+		add_filter( 'cron_schedules', array( __CLASS__ , 'add_new_intervals' ) );
+	}
+
+	/**
+	*  
+	*/
+	public static function add_new_intervals($schedules) {
+		$schedules['10min'] = array(
+			'interval' => 60 * 10,
+			'display' => __('Every 10 Min')
+		);	
+
+		return $schedules;
+	}
+	
+	/**
+	*  
+	*/
+	public static function debug_cron() {
+		if ( isset($_GET['debugyt']) ) {
+			do_action('check_for_liked_videos');
+		}	
+	}
+	
+	/**
+	*  
+	*/
+	public static function check_for_liked_videos() {
+		$settings = YT_Liked_Videos_Settings::get_settings();
+		
+		if ( empty( $settings['access_token'] ) ) {
+			return;
+		}
+		
+		$videos = Youtube_Liked_Videos_Connect::get_liked_videos();
+
+		self::$settings = YT_Liked_Videos_Settings::get_settings();
+		self::$history = get_option( 'yt_liked_videos' , array() );
+		
+		foreach ($videos as $key => $video) {
+			$video_id = $video['id'];
+			$link = 'http://www.youtube.com/watch?v=' . $video_id;
+			$data = $video['modelData'];
+			$data['id'] = $video_id;
+
+			/* check if already processed */
+			if ( self::check_history( $video_id ) ) {
+				continue;
+			}
+			
+			/* prepare post body and title */
+			$postbody = self::replace_tokens(self::$settings['postbody'] , $data );
+			$title = self::replace_tokens(self::$settings['title'] , $data );
+			
+			/* create post */
+			$post_id = wp_insert_post(
+				array(
+					'comment_status'	=>	'closed',
+					'ping_status'		=>	'closed',
+					'post_title'		=>	$title,
+					'post_content'		=>	$postbody,
+					'post_status'		=>	'publish',
+					'post_type'		=>	'liked-videos'
+				)
+			);
+
+			self::$history[] = $video_id;
+		}
+		
+		self::update_history();
+	}
+	
+	/**
+	*  replace tokens with content
+	*/
+	public static function replace_tokens( $string , $data ) {
+		$string = str_replace( '{{video-title}}' , $data['snippet']['title'], $string );
+		$string = str_replace( '{{video-id}}' , $data['id'], $string );
+		$string = str_replace( '{{video-description}}' , $data['snippet']['description'], $string );
+		$string = str_replace( '{{thumbnail-default}}' , $data['snippet']['thumbnails']['default']['url'], $string );
+		$string = str_replace( '{{thumbnail-medium}}' , $data['snippet']['thumbnails']['medium']['url'], $string );
+		$string = str_replace( '{{thumbnail-high}}' , $data['snippet']['thumbnails']['high']['url'], $string );
+		$string = str_replace( '{{iframe-embed}}' , $data['player']['embedHtml'], $string );
+		
+		return $string;
+	}
+	
+	/**
+	*  check to see if post already exist
+	*/
+	public static function check_history( $video_id ) {
+
+		if (in_array($video_id , self::$history)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	*  updates history
+	*/
+	public static function update_history(  ) {
+		update_option( 'yt_liked_videos', self::$history, false );
+	}	
+}
+
+new Youtube_Liked_Cron;
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== WordPress Import Youtube Liked Videos ===
+
+Contributors: Hudson Atwell
+Donate link: mailto:hudson@paypal.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: youtube, liked videos, youtube api 3.0, import, automated, cron
+Requires at least: 3.8
+Tested up to: 4.2
+Stable Tag: 1.0.2
+
+Automatically import liked Youtube videos into custom post type
+
+== Description ==
+
+This plugin helps a user connect to their YoutTube Account and import their recently liked videos. It's powered by the WordPress cronjob system to automatically update itself. 
+
+The plugin can also serve as an example to other developers on how to connect to the Google API. 
+
+[Follow Development on GitHub ](https://github.com/atwellpub/wordpress-import-liked-youtube-videos "Follow & Contribute to core development on GitHub")
+ |
+[Follow Development on Twitter ](https://twitter.com/atwellpub )
+
+
+== Installation ==
+
+1. Upload `youtube-liked-videos` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+* Hello! 
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0.2 =
+
+Released
